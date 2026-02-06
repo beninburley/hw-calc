62e13907241acdd0c8e3a0fb4bfb307258bf4770
beninburley <130599399+beninburley@users.noreply.github.com>
2026-02-06 14:11:37 -0700

    Explain calc state

--- src/types.ts
+++ src/types.ts
@@ -13,6 +13,10 @@ export type PendingOperation =
   | { preValue: number; operand: Operation | null }
   | { preValue: null };
 
+// Calculator State requires two fields: the display and the pending operation
+// (within the operation exists the previous value and the operand).
+// These fields are what we used previously with our useState hooks, so I just
+// imported them into the CalculatorState.
 export type CalculatorState = {
   displayState: string;
   pendOp: PendingOperation;
